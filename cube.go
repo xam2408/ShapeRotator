1723ff22d6ac412dd1d48ab11010f8260d590d41
xam2408 <maksymilian2408@gmail.com>
2026-01-11 21:13:08 +0100

    Add W axis rotation for simulating tessaract

--- cube.go
+++ cube.go
@@ -9,6 +9,10 @@ import (
     "html/template"
 )
 
+type Point4D struct{
+    X, Y, Z, W float64
+}
+
 type Point3D struct{
     X, Y, Z float64
 }
@@ -19,57 +23,68 @@ type Screen struct{
     Color string `json:"color"`
 }
 
-func getCube() []Point3D{
-    return []Point3D{
-
-//cubeoutside
-        { -1, -1, -1 },
-        {  1, -1, -1 },
-        {  1,  1, -1 },
-        { -1,  1, -1 },
-        { -1, -1,  1 },
-        {  1, -1,  1 },
-        {  1,  1,  1 },
-        { -1,  1,  1 },
-
-//cube inside
-        { -0.5, -0.5, -0.5 },
-        {  0.5, -0.5, -0.5 },
-        {  0.5,  0.5, -0.5 },
-        { -0.5,  0.5, -0.5 },
-        { -0.5, -0.5,  0.5 },
-        {  0.5, -0.5,  0.5 },
-        {  0.5,  0.5,  0.5 },
-        { -0.5,  0.5,  0.5 },
+func getTessaract() []Point4D{
+    var points []Point4D
+    for _, x := range []float64{-1,1}{
+        for _, y := range []float64{-1,1}{
+            for _, z := range []float64{-1,1}{
+                for _, w := range []float64{-1,1}{
+                    points = append(points, Point4D{x, y, z, w})
+                }
+            }
+        }
+    }
+    return points
+}
 
 
+
+func rotateZW(p Point4D, angle float64) Point4D{
+    nz := p.Z*math.Cos(angle) - p.W*math.Sin(angle) 
+    nw := p.Z*math.Sin(angle) + p.W*math.Cos(angle) 
+    return Point4D{
+        X: p.X,
+        Y: p.Y,
+        Z: nz,
+        W: nw,
     }
 }
 
-
-func rotateY(p Point3D, angle float64) Point3D{
-    nx := p.X*math.Cos(angle) - p.Z*math.Sin(angle) 
-    nz := p.X*math.Sin(angle) + p.Z*math.Cos(angle) 
-    return Point3D{
+func rotateXW(p Point4D, angle float64) Point4D{
+    nx := p.X*math.Cos(angle) - p.W*math.Sin(angle) 
+    nw := p.X*math.Sin(angle) + p.W*math.Cos(angle) 
+    return Point4D{
         X: nx,
         Y: p.Y,
-        Z: nz,
+        Z: p.Z,
+        W: nw,
     }    
 }
 
-func rotateX(p Point3D, angle float64) Point3D{
-    ny := p.Y*math.Cos(angle) - p.Z*math.Sin(angle) 
-    nz := p.Y*math.Sin(angle) + p.Z*math.Cos(angle) 
+func rotateXY(p Point3D, angle float64) Point3D{
+    nx := p.X*math.Cos(angle) - p.Y*math.Sin(angle) 
+    ny := p.X*math.Sin(angle) + p.Y*math.Cos(angle) 
     return Point3D{
-        X: p.X,
+        X: nx,
         Y: ny,
-        Z: nz,
+        Z: p.Z,
     }    
      
 }
 
+func proj4Dto3D(p Point4D) Point3D{
+    distance := 2.5
+    wFactor := 1.0 / (distance - p.W)
+    return Point3D{
+        X: p.X * wFactor,
+        Y: p.Y * wFactor,
+        Z: p.Z * wFactor,
+
+    }
+}
+
 func proj(p Point3D) Screen{
-    scale := 300.0
+    scale := 400.0
     distance := 4.0
     center := 300.0
 
@@ -86,16 +101,18 @@ func proj(p Point3D) Screen{
 }
 
 
-func prepareCube(angleX, angleY float64) []Screen{
-    rawPoints := getCube()
+func prepareTessaract(angle4D, angle3D float64) []Screen{
+    rawPoints := getTessaract()
     var result []Screen
 
-    for _, p := range rawPoints{
-        pRotatedX := rotateX(p, angleX)
-        pRotatedY := rotateY(pRotatedX, angleY)
+    for _, p4 := range rawPoints{
+        pRotated4D := rotateZW(p4, angle4D)
 
-        p2D := proj(pRotatedY)
-        result = append(result, p2D)
+        p3 := proj4Dto3D(pRotated4D)
+        pRotated3D := rotateXY(p3, angle3D)
+
+        p2 := proj(pRotated3D)
+        result = append(result, p2)
     }
 
     return result
@@ -120,10 +137,10 @@ func indexHandler(w http.ResponseWriter, r *http.Request){
 
 func dataHandler(w http.ResponseWriter, r *http.Request){
     t := time.Now().UnixMilli()
-    angleY := float64(t)/1000.0
-    angleX := float64(t)/1500.0
+    angle4D := float64(t)/1500.0
+    angle3D := float64(t)/3000.0
 
-    readyFrame := prepareCube(angleX, angleY)
+    readyFrame := prepareTessaract(angle4D, angle3D)
     
     w.Header().Set("Content-Type", "application/json")
     json.NewEncoder(w).Encode(readyFrame)
